e6f2e308c826aea4df17ae8fda453baa9feba425
apotanin <apotanin@alteryx.com>
2017-11-12 16:47:31 +0200

    lesson-2 tasks

---
+++ src/lesson-2/tasks/task-1/after/index.js
@@ -0,0 +1,15 @@
+export function findString(text, pattern) {
+    return text.split(pattern).length-1;
+}
+
+// export function findString(text, pattern) {
+//     let num = 0;
+//     for (let i = 0; i < text.length; i++) {
+//         pattern.lastIndex = i;
+//         if (pattern.exec(text)) {
+//             num++;
+//             i = pattern.lastIndex;
+//         }
+//     }
+//     return num;
+// }
