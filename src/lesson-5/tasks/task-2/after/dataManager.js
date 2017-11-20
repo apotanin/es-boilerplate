f14cefa869579fa48617f3adf5b803530ca856d1
apotanin <apotanin@alteryx.com>
2017-11-20 18:43:32 +0200

    lesson-5 tasks small fix

--- src/lesson-5/tasks/task-2/after/dataManager.js
+++ src/lesson-5/tasks/task-2/after/dataManager.js
@@ -5,25 +5,24 @@ import {getEntity, validator} from '../../task-2/helper'
 export function DataManager() {
     this.data = [];
 
+    this.add = (entity) => {
+        validator(entity);
+        if (getEntity(entity.id, this.data)) throw new Error('entity has been placed previously');
+        this.data.push(entity);
+    };
+
+    this.getCount = () => this.data.length;
+
+    this.getEntities = () => this.data.length ? this.data : null;
+
+    this.getEntityById = (id) => getEntity(id, this.data);
+
+    this.getFirstEntity = () => this.data.length ? this.data[0] : null;
+
+    this.getLastEntity = () => this.data.length ? this.data[this.data.length - 1] : null;
+
+    this.filter = (callback = () => false) => {
+        if (typeof callback !== 'function') throw new Error('callback should be a function');
+        return this.data.filter(callback);
+    };
 }
-
-DataManager.prototype.add = function (entity){
-    validator(entity);
-    if (getEntity(entity.id, this.data)) throw new Error('entity has been placed previously');
-    this.data.push(entity);
-};
-
-DataManager.prototype.getCount = function () { return this.data.length };
-
-DataManager.prototype.getEntities = function () { return this.data.length ? this.data : null;};
-
-DataManager.prototype.getEntityById = function (id) { return getEntity(id, this.data);};
-
-DataManager.prototype.getFirstEntity = function () { return this.data.length ? this.data[0] : null;};
-
-DataManager.prototype.getLastEntity = function () { return this.data.length ? this.data[this.data.length - 1] : null};
-
-DataManager.prototype.filter = function (callback = () => false) {
-    if (typeof callback !== 'function') throw new Error('callback should be a function')
-    return this.data.filter(callback);
-};
